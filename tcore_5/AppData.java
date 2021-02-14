0c51e558bd7cdf0c30d7067d59cb1a159606e371
Alina <alishkasun@gmail.com>
2021-02-14 16:10:59 +0400

    HW_5

---
+++ tcore_5/AppData.java
@@ -0,0 +1,24 @@
+package tcore_5;
+
+public class AppData {
+
+    private String[] header;
+
+    private int[][] data;
+
+    public AppData(String[] header, int[][] data) {
+        this.header = header;
+        this.data = data;
+    }
+
+    public String[] getHeader() {
+        return header;
+    }
+
+    public int[][] getData() {
+        return data;
+    }
+}
+
+
+
